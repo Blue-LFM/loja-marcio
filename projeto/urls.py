73bcd3ee15b7cfd915b6820a0258fbe979b8cd6b
Blue-LFM <leticiamartinazzo1@gmail.com>
2024-11-25 15:55:56 -0300

    Update urls.py

--- projeto/urls.py
+++ projeto/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from projeto.settings import DEBUG
 from django.conf import settings
 from django.conf.urls.static import static
-from aplicacao.views import index
+from aplicacao.views import index, index2, index3, index4, index5
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('1', index2),
+    path('2', index3),
+    path('3', index4),
+    path('4', index5),
 ]
 
 
