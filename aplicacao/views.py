83293b0686c78f1e1bac15494d1fc3458a772505
Blue-LFM <leticiamartinazzo1@gmail.com>
2024-11-25 15:54:55 -0300

    Update views.py

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'aplicacao/index.html')
+       
+def index2(request):
+    return render(request, 'aplicacao/index2.html')
+
+def index3(request):
+    return render(request, 'aplicacao/index3.html')
+
+def index4(request):
+    return render(request, 'aplicacao/index4.html')
+
+def index5(request):
+    return render(request, 'aplicacao/index5.html')
